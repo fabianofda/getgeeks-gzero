f252ee34d8fd1c86f4f1734a34ee5c82945a12a4
fabianofda <54209603+fabianofda@users.noreply.github.com>
2022-05-19 10:42:19 -0300

    teste chmod +x run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,4 @@
 # chmod +x run.sh - > comando para dar permissao de exec do arquivo
-#robot -d ./logs tests  
-
-robot -x xunit.xml -d ./logs tests  
+#robot -d ./logs tests 
+chmod +x run.sh
+robot --xunit xunit.xml -d ./logs tests  
